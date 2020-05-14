0cfa1802be13328de0e283cba4530cbe8715564d
Gur Kunst <gkunst85@hotmail.com>
2020-05-15 00:55:37 +0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -15,44 +15,55 @@ using Persistence;
 
 namespace API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt =>
+      public class Startup
+      {
+            public Startup(IConfiguration configuration)
             {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+                  Configuration = configuration;
             }
 
-            // Redirect http to https
-            // app.UseHttpsRedirection();
+            public IConfiguration Configuration { get; }
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            // This method gets called by the runtime. Use this method to add services to the container.
+            public void ConfigureServices(IServiceCollection services)
             {
-                endpoints.MapControllers();
-            });
-        }
-    }
+                  services.AddDbContext<DataContext>(opt =>
+                  {
+                        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+                  });
+                  services.AddCors(opt =>
+                  {
+                        opt.AddPolicy("CorsPolicy", policy =>
+                        {
+                              policy.AllowAnyHeader()
+                                    .AllowAnyMethod()
+                                    .WithOrigins("http://localhost:3000");
+                        });
+                  });
+                  services.AddControllers();
+            }
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                  if (env.IsDevelopment())
+                  {
+                        app.UseDeveloperExceptionPage();
+                  }
+
+                  // Redirect http to https
+                  // app.UseHttpsRedirection();
+
+                  app.UseRouting();
+
+                  app.UseAuthorization();
+
+                  app.UseCors("CorsPolicy");
+
+                  app.UseEndpoints(endpoints =>
+                  {
+                        endpoints.MapControllers();
+                  });
+            }
+      }
 }
--- Readme.txt
+++ Readme.txt
@@ -13,6 +13,21 @@ Business Logic Layer
 Commands
 ===========================
 
-migrations:
-dotnet ef migrations add [migrationName] -p [projectName] -s [startup projectName]
-dotnet ef migrations add SeedValues -p Persistence/ -s API/
+NPM
+
+      npx create-react-app client-app --use-npm --typescript
+      npm i (install)
+      npm start
+
+.NET
+
+
+Migrations:
+      
+      dotnet ef migrations add [migrationName] -p [projectName] -s [startup projectName]
+      dotnet ef migrations add SeedValues -p Persistence/ -s API/
+
+
+GIT
+
+      git clone https://github.com/gkunst85/Reactivities.git
