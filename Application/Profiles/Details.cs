6f3cbf7d449df87192f4b1ce7bc3df9c2e01a999
Gur Kunst <gkunst85@hotmail.com>
2020-05-29 01:38:13 +0300

    End or lecture 223 section 16

---
+++ Application/Profiles/Details.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles
+{
+      public class Details
+      {
+            public class Query : IRequest<Profile>
+            {
+                  public string UserName { get; set; }
+            }
+
+            public class Handler : IRequestHandler<Query, Profile>
+            {
+                  private readonly DataContext _context;
+
+                  public Handler(DataContext context)
+                  {
+                        _context = context;
+                  }
+
+                  public async Task<Profile> Handle(Query request, CancellationToken cancellationToken)
+                  {
+                        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.UserName);
+                        if (user == null)
+                              throw new RestException(HttpStatusCode.NotFound, new { Profile = "Could not find username" });
+
+                        return new Profile
+                        {
+                              UserName = user.UserName,
+                              DisplayName = user.DisplayName,
+                              MainImage = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                              Photos = user.Photos,
+                              Bio = user.Bio
+                        };
+                  }
+            }
+      }
+}
---
+++ Application/Profiles/Profile.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Domain;
+
+namespace Application.Profiles
+{
+      public class Profile
+      {
+            public string DisplayName { get; set; }
+            public string UserName { get; set; }
+            public string MainImage { get; set; }
+            public string Bio { get; set; }
+            public ICollection<Photo> Photos { get; set; }
+      }
+}
