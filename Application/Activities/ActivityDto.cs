a750c232179d8527a100d8e36e5ad58454a6a86e
Gur Kunst <gkunst85@hotmail.com>
2020-05-31 00:38:21 +0300

    End of lecture 249 section 19

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,21 +1,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Application.Comments;
 
 namespace Application.Activities
 {
-    public class ActivityDto
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public DateTime Date { get; set; }
-        public string City { get; set; }
-        public string Venue { get; set; }
+      public class ActivityDto
+      {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
 
-        // Will return the data as attendees & also be recognized by AutoMapper
-        [JsonPropertyName("attendees")]
-        public ICollection<AttendeeDto> UserActivities { get; set; }
-    }
+            // Will return the data as attendees & also be recognized by AutoMapper
+            [JsonPropertyName("attendees")]
+            public ICollection<AttendeeDto> UserActivities { get; set; }
+            public ICollection<CommentDto> Comments { get; set; }
+      }
 }
--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 
 namespace Domain
 {
-    public class Activity
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public DateTime Date { get; set; }
-        public string City { get; set; }
-        public string Venue { get; set; }
+      public class Activity
+      {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
 
-        // Defines the relationship between our Activity class and the UserActivities class
-        public virtual ICollection<UserActivity> UserActivities { get; set; }
-    }
+            // Defines the relationship between our Activity class and the UserActivities class
+            public virtual ICollection<UserActivity> UserActivities { get; set; }
+            public virtual ICollection<Comment> Comments { get; set; }
+      }
 }
